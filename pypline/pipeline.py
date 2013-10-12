69e8f6cdb1a26c7be3a1a8b6e938407ea5eca1b1
Donagh Hatton <donaghhatton@gmail.com>
2013-10-12 10:23:35 +0100

    pipeline validate reorganised

--- pypline/pipeline.py
+++ pypline/pipeline.py
@@ -12,19 +12,28 @@ class BundlePipeController(PipeController, BundleMixin):
 
 
 class Pipeline(object):
-    def __init__(self, message, tasks=[]):
-        self.message = message
+    def __init__(self, tasks=[]):
         self.__tasks = tasks
+        self.__validate()
 
-    def validate(self):
-        return all([callable(t) for t in self.__tasks])
+    def add_task(self, task):
+        self.__tasks.append(task)
 
-    def execute(self):
-        if not self.validate():
+
+    def add_task_after(self, task, cls):
+        index = next(i for (i, x) in enumerate(self.__tasks)
+            if x.__class__ == cls)
+        self.__tasks.insert(task, index)
+
+
+    def __validate(self):
+        if not all([callable(t) for t in self.__tasks]):
             raise TypeError("Pipeline failed to validate, " \
                 " all Tasks must be Callable")
+
+    def execute(self, message):
         for task in self.__tasks:
-            task(self.message, self)
+            task(message, self)
 
 
 if __name__ == "__main__":
