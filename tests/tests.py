73bcc2d5b3e1a9be49912520caec0a4d9e7d3642
Donagh Hatton <donaghhatton@gmail.com>
2013-10-16 15:15:20 +0100

    tests rewritten to use unittest correctly

--- tests/tests.py
+++ tests/tests.py
@@ -1,72 +1,33 @@
+import unittest
 import sys
 sys.path.append("..")
-import pypline.pipeline
-import timeit
-reload(pypline.pipeline)
-reload(pypline.task)
-from pypline.pipeline import Pipeline, PipeController, RepeatingPipeline
-from pypline.task import AsyncTask
+import pypline
+
+class Cap(pypline.Task):
+    def __call__(self, message, pipeline):
+        return message.capitalize()
+
+def rev(message, pipeline):
+    return message[-1::-1]
+
+
+def make_simple_pipe():
+    return pypline.Pipeline([Cap(), rev])
+
+
+class TestPipeLine(unittest.TestCase):
+    def test_empty(self):
+        p = pypline.Pipeline()
+        p.execute()
+
+    def test_simple(self):
+        p = make_simple_pipe()
+        self.assertTrue(p.execute("hello") == "olleH")
+
+    def test_non_callable(self):
+        self.assertRaises(TypeError, pypline.Pipeline, [1, 2])
 
 
 if __name__ == "__main__":
-    def cap(message, pipeline):
-        assert type(message) == type("")
-        return message.capitalize()
-
-    def rev(message, pipeline):
-        return message[-1::-1]
-
-    def wait(message, pipeline):
-        import time
-        time.sleep(0)
-        return message
-
-    class GetKeyboardInput(AsyncTask):
-        def __call__(self, message, pipeline, callback):
-            from threading import Thread
-            def get_inp():
-                x = raw_input()
-                callback(x)
-            Thread(target=get_inp).start()
-            print "LOOK MA THREADS"
-
-
-    pipe1 = Pipeline([GetKeyboardInput(), cap, rev])
-    y = pipe1.execute()
-    print y
-
-    # class LengthController(PipeController):
-    #     def done(self, message):
-    #         print "Cont: %s" % message
-    #         return len(message) > 10000
-
-    # def addLetter(message, pipeline):
-    #     import random, string
-    #     pipeline(message + random.choice(string.lowercase))
-
-    # def addLetter2(message):
-    #     import random, string
-    #     return message + random.choice(string.lowercase)
-
-
-    # def init(message, pipeline):
-    #     pipeline(message)
-
-
-    # def final(message, pipeline):
-    #     pipeline(message)
-
-
-    # pipe2 = RepeatingPipeline(LengthController(), [init], [addLetter], [final])
-    # now = timeit.default_timer()
-    # y = pipe2.execute("hello")
-    # print y
-    # print timeit.default_timer() - now
-
-    # now = timeit.default_timer()
-    # msg = "hello"
-    # cont = LengthController()
-    # while not cont.done(msg):
-    #     msg = addLetter2(msg)
-    # print msg
-    # print timeit.default_timer() - now
+    unittest.main()
+    pass
