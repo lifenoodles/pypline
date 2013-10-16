971cc1db792a4ab1447bcf088a98b87fac699be2
Donagh Hatton <donaghhatton@gmail.com>
2013-10-16 15:14:57 +0100

    defining top level API in __init__

---
+++ pypline/__init__.py
@@ -0,0 +1,7 @@
+from pipeline import Pipeline
+from pipeline import RepeatingPipeline
+from pipeline import AsyncMixin
+from task import Task
+from task import AsyncTask
+
+__all__ = ["Pipeline", "RepeatingPipeline", "AsyncMixin", "Task", "AsyncTask"]
