3182a4a80133e50c4164c157fc4138b9a3025c6a
Donagh Hatton <donaghhatton@gmail.com>
2013-11-06 10:41:07 +0000

    build pipeline added to manager

--- pypline/managers.py
+++ pypline/managers.py
@@ -1,6 +1,8 @@
 import pipeline
 
 
+class NotRegisteredBuilderException(Exception): pass
+
 class PipelineDefinition(object):
     def __init__(self, name, tasks):
         self.name = name
@@ -28,7 +30,32 @@ class PipeLineManager(object):
     def __init__(self, pipelines=[]):
         self.pipelines = []
         self.pipeline_defs = []
+        self.task_builders = {}
 
     def add_pipeline(self, pipeline):
         self.pipelines.append(pipeline)
+        return self
+
+    def add_task_builder(self, builder):
+        builder_name = str(builder).split('.')[-1]
+        self.task_builders[builder_name] = builder
+        return self
+
+    def build_pipeline(self, builder_names, args):
+        builders = []
+        for i, builder in enumerate(builder_names):
+            if self.task_builders.has_key(builder):
+                builders[i] = self.task_builders[builder]
+            else:
+                raise NotRegisteredBuilderException("No registered "
+                        "builder with that name")
+
+        for i, (builder, args) in enumerate(zip(builders, args)):
+            builders[i] = builder(*args)
+
+        pipe = pipeline.Pipeline(builders)
+        self.pipelines.append(pipe)
+        # return self or pipe here?
+        return self
+
 
