81a6a99d1c21291c7114d298acb4e5c2f3e0a39f
Donagh Hatton <donaghhatton@gmail.com>
2013-11-07 14:57:33 +0000

    refactored public api

--- pypline/__init__.py
+++ pypline/__init__.py
@@ -1,9 +1,13 @@
 from pipeline import Pipeline
+from pipeline import AsyncPipeline
 from pipeline import RepeatingPipeline
-from pipeline import AsyncMixin
+from pipeline import AsyncRepeatingPipeline
 from task import Task
 from task import AsyncTask
+from managers import PipelineBuilder
+from managers import RepeatingPipelineBuilder
 from managers import PipeLineManager
 
-__all__ = ["Pipeline", "RepeatingPipeline", "AsyncMixin", "Task", \
-        "AsyncTask", "PipeLineManager"]
+__all__ = ["Pipeline", "AsyncPipeline", "RepeatingPipeline", \
+        "AsyncRepeatingPipeline", "Task", "AsyncTask", \
+        "PipelineBuilder", "RepeatingPipelineBuilder", "PipeLineManager"]
