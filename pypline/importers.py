6f99de8ba6370808c0406c9d1ca5628205bad8b6
Donagh Hatton <donaghhatton@gmail.com>
2013-11-07 17:27:13 +0000

    building pipeline from py conf file

--- pypline/importers.py
+++ pypline/importers.py
@@ -1,4 +1,5 @@
 import abc
+import itertools
 import imp
 import inspect
 import managers
@@ -47,10 +48,50 @@ class ManagerBuilder(object):
 
 
 class PythonManagerBuilder(ManagerBuilder):
-    def build_repeating(self, pipespec):
-        pass
+    def build_repeating(self, spec, tasks):
+        task_names = set()
+        parameter_lists = []
+        parameter_names = []
 
-    def build_pipeline(self, pipespec):
+        def process_task(task):
+            inc = 1
+            name = task["name"]
+            modified_name = name
+            while modified_name in task_names:
+                modified_name = name + str(inc)
+                inc += 1
+            task_names.add(modified_name)
+            parameter_lists.append(task["params"])
+            parameter_names.append(modified_name)
+            return (tasks[name], modified_name)
+
+        pipe_name = spec["name"]
+        controller = process_task(spec["controller"])
+        initialisers, main_tasks, finalisers = [], [], []
+
+        for init in spec["initialisers"]:
+            initialisers.append(process_task(init))
+        for task in spec["tasks"]:
+            main_tasks.append(process_task(task))
+        for final in spec["finalisers"]:
+            finalisers.append(process_task(final))
+
+        builder = managers.RepeatingPipelineBuilder(pipe_name, controller, \
+                initialisers, main_tasks, finalisers)
+
+        configs = []
+        print parameter_names
+        print parameter_lists
+
+        config_combos = [c for c in itertools.product(*parameter_lists)]
+        print config_combos
+        for combo in config_combos:
+            config = managers.PipelineConfiguration()
+            for name, c in zip(parameter_names, combo):
+                config[name] = c
+        return (builder, configs)
+
+    def build_pipeline(self, spec):
         pass
 
     def build_manager(self, filename):
@@ -64,16 +105,15 @@ class PythonManagerBuilder(ManagerBuilder):
         manager = managers.PipeLineManager()
         for pipe in spec.pipelines:
             if pipe.has_key("controller"):
-                pipe_builder, configs = self.build_repeating(pipe)
+                pipe_builder, configs = self.build_repeating(pipe, \
+                        importer.tasks)
             else:
-                pipe_builder, configs = self.build_pipeline(pipe)
+                pipe_builder, configs = self.build_pipeline(pipe, \
+                        importer.tasks)
             manager.register_pipeline_builder(pipe_builder)
             for conf in configs:
                 manager.register_configuration(pipe_builder.name, conf)
         return manager
 
-
-
-
 if __name__ == "__main__":
-    PythonManagerBuilder().build_manager("../tests/test_pipeline_conf.py")
+    x = PythonManagerBuilder().build_manager("../tests/test_pipeline_conf.py")
