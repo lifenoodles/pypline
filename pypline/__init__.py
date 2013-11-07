aac291859dedfecb8a233827ce578d39ce290a91
Donagh Hatton <donaghhatton@gmail.com>
2013-11-07 11:00:31 +0000

    pipelineManager added to top level api

--- pypline/__init__.py
+++ pypline/__init__.py
@@ -3,5 +3,7 @@ from pipeline import RepeatingPipeline
 from pipeline import AsyncMixin
 from task import Task
 from task import AsyncTask
+from managers import PipeLineManager
 
-__all__ = ["Pipeline", "RepeatingPipeline", "AsyncMixin", "Task", "AsyncTask"]
+__all__ = ["Pipeline", "RepeatingPipeline", "AsyncMixin", "Task", \
+        "AsyncTask", "PipeLineManager"]
