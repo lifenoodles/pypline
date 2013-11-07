3edb141c923cc5ddbcd00b170421ec0629daae5b
Donagh Hatton <donaghhatton@gmail.com>
2013-11-07 10:58:28 +0000

    added unit test for repeating pipeline

--- tests/tests.py
+++ tests/tests.py
@@ -5,21 +5,39 @@ import pypline
 
 
 class Initialiser(pypline.Task):
-    def __call__(self, message, pipeline):
+    def process(self, message, pipeline):
         return "INIT\n"
 
 
+class Finaliser(pypline.Task):
+    def process(self, message, pipeline):
+        return message + "FINAL\n"
+
+
 class ControllerN(pypline.Task):
-    pass
+    def __init__(self, n):
+        self.n = n
+
+    def process(self, message, pipeline):
+        self.n -= 1
+        return self.n < 0
+
+
+class GenericTask(pypline.Task):
+    def __init__(self, number):
+        self.number = number;
+
+    def process(self, message, pipeline):
+        return message + "TASK %d\n" % self.number
 
 
 class Cap(pypline.Task):
-    def __call__(self, message, pipeline):
+    def process(self, message, pipeline):
         return message.capitalize()
 
 
 class Reverse(pypline.Task):
-    def __call__(self, message, pipeline):
+    def process(self, message, pipeline):
         return message[-1::-1]
 
 
@@ -27,7 +45,7 @@ class TimesN(pypline.Task):
     def __init__(self, n=2):
         self.n = n
 
-    def __call__(self, message, pipeline):
+    def process(self, message, pipeline):
         return message * self.n
 
 
@@ -43,18 +61,13 @@ class TestPipeLine(unittest.TestCase):
     def test_non_callable(self):
         self.assertRaises(TypeError, pypline.Pipeline, [1, 2])
 
+    def test_repeating(self):
+        p = pypline.RepeatingPipeline(ControllerN(3), \
+                [Initialiser()], \
+                [GenericTask(1), GenericTask(2)], \
+                [Finaliser()])
+        self.assertTrue(p.execute() == "INIT\nTASK 1\nTASK 2\nTASK 1\n" \
+                "TASK 2\nTASK 1\nTASK 2\nFINAL\n")
 
 if __name__ == "__main__":
     unittest.main()
-
-    class MyStr(pypline.Task):
-        def __call__(self, message, pipe):
-            return str(message)
-
-    class MyInt(pypline.Task):
-        def __call__(self, message, pipe):
-            return int(message)
-
-    p = pypline.Pipeline([MyStr(), MyInt()])
-    p.add_task_after(MyStr(), MyInt)
-    print p.execute(1)
