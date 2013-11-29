1ab1b9cde0cf48f5f6b491c6e230ffadc34871df
Donagh Hatton <donaghhatton@gmail.com>
2013-11-29 16:01:05 +0000

    updated public api

--- pypline/__init__.py
+++ pypline/__init__.py
@@ -2,8 +2,11 @@ from pipeline import Pipeline
 from pipeline import AsyncPipeline
 from pipeline import RepeatingPipeline
 from pipeline import AsyncRepeatingPipeline
+from pipeline import DependencyError
 from task import Task
 from task import AsyncTask
+from task import requires
+from task import provides
 from managers import PipelineBuilder
 from managers import RepeatingPipelineBuilder
 from managers import PipeLineManager
@@ -11,6 +14,6 @@ from importers import PythonManagerBuilder
 from importers import YamlManagerBuilder
 
 __all__ = ["Pipeline", "AsyncPipeline", "RepeatingPipeline",
-           "AsyncRepeatingPipeline", "Task", "AsyncTask",
+           "AsyncRepeatingPipeline", "DependencyError", "Task", "AsyncTask",
            "PipelineBuilder", "RepeatingPipelineBuilder", "PipeLineManager",
-           "PythonManagerBuilder", "YamlManagerBuilder"]
+           "PythonManagerBuilder", "YamlManagerBuilder", "requires", "provides"]
