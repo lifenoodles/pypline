7add69a6bd6025e9c4852011331f99c27888bb40
Donagh Hatton <donaghhatton@gmail.com>
2013-11-29 16:02:29 +0000

    minor version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Pypline',
-    version='0.1.1',
+    version='0.1.2',
     author='Donagh Hatton',
     author_email='donaghhatton@gmail.com',
     packages=['pypline'],
